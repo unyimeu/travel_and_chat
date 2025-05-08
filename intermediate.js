20d753f2fe5d168efdf4b747f7df0b258fbb8ccb
Unyime Usua <unyime@mit.edu>
2025-05-07 21:07:28 -0400

    hopefully fixxes the bug

--- intermediate.js
+++ intermediate.js
@@ -1,10 +1,14 @@
 import { createApp } from "vue";
+import { GraffitiLocal } from "@graffiti-garden/implementation-local";
+import { GraffitiPlugin } from "@graffiti-garden/wrapper-vue";
 
+const graffiti = new GraffitiLocal();
 createApp({
   methods: {
     openChats() {
-      window.location.href = "index.html";
-    }
-  }
+      window.location.href = "home.html";
+    },
+  },
 })
+  .use(GraffitiPlugin, { graffiti })
   .mount("#intermediateApp");
